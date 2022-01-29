0106f9b9e3b01ad7476680d7d7820811b4cde6a0
lukas.vantel <lukas.vandyke@gmail.com>
2022-01-29 09:39:15 -0700

    Added player selection scene/script; stole the background from the main menu

---
+++ src/Player/PlayerSelect.gd
@@ -0,0 +1,12 @@
+extends Control
+
+export var start_bg = false setget _start_bg
+
+func _start_bg(var _val):
+	if $Background:
+		$Background.material.set_shader_param("play", true)
+
+func _on_LineEdit_text_changed(new_text):
+	if new_text == "start" or new_text == "Start":
+		get_tree().quit()
+		#Change to next scene
