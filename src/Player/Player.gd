6fe75e5c787a707544aa40394497da3d031496e7
lukas.vantel <lukas.vandyke@gmail.com>
2022-01-29 12:07:17 -0700

    Changed player to a kinematic body 2d; added camera, collision shape, and sprite with animation for run/jump/fall to the player

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -1,15 +1,23 @@
 extends Control
 
+export(Array, Texture) var PossibleTextures
+var currentTexture = 0
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$CharacterControl/CharacterSprite.set_texture(PossibleTextures[currentTexture])
+	$CharacterControl/CharacterSprite/AnimationPlayer.play("run")
 
+#Change the sprite sheet used in the player's animations
+func _change_sprite(newSprite):
+	currentTexture = newSprite
+	
+	#Ensure the value passed in is not outside the bounds of the possible textures
+	if (currentTexture < 0):
+		currentTexture = 0
+	if (currentTexture >= PossibleTextures.size()):
+		currentTexture = PossibleTextures.size() - 1
+		
+	$CharacterControl/CharacterSprite.set_texture(PossibleTextures[currentTexture])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
