8e533d49be2df621f5c1d561f6724e2463c12cb2
lukas.vantel <lukas.vandyke@gmail.com>
2022-01-30 09:55:24 -0700

    Set should ghost when all frequency is 100; currently getting a crash when instancing ice tile for some reason

--- src/Menus/TrapSelection.gd
+++ src/Menus/TrapSelection.gd
@@ -11,5 +11,6 @@ func _create_level_dict():
 
 func _on_LineEdit_text_changed(new_text):
 	if new_text.to_lower() == "start":
+		WizLord.set_should_ghost($Traps/All.freq == 100)
 		WizLord.set_traps(_create_level_dict())
 		WizLord.wizload("res://Managers/GameManager.tscn")
