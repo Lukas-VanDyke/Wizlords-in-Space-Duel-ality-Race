5f55945f09132aa83e5903c0f100808c80427c58
clayjohn <claynjohn@gmail.com>
2022-01-30 15:04:26 -0800

    add visual polish

--- src/Traps/Crate.gd
+++ src/Traps/Crate.gd
@@ -1,9 +1,10 @@
 extends Node2D
 
 var triggered = false
+var finished = false
 
 func _on_Area2D_body_entered(body):
-	if body is KinematicBody2D and not triggered:
+	if body is KinematicBody2D and not triggered and not finished:
 		$Sprite.play()
 		$Sprite/Sprite.play()
 		$Sprite/Sprite2.play()
@@ -11,6 +12,7 @@ func _on_Area2D_body_entered(body):
 		$Explosion.play()
 		$Fire.emitting = true
 		body.emit_signal("hit_trap", "explosion")
+		finished = true
 
 
 func _on_Trigger_body_entered(body):
@@ -23,5 +25,6 @@ func _on_Trigger_body_entered(body):
 
 
 func _on_DeathZone_body_entered(body):
-	if body is KinematicBody2D and triggered:
+	if body is KinematicBody2D and triggered and not finished:
 		body.emit_signal("hit_trap", "explosion")
+		finished = true
