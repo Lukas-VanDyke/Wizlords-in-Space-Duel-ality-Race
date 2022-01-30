d46c2d28e200f6260b45c42aa115359f7f7a13f5
CalebJohn <calebjohn4@gmail.com>
2022-01-29 16:49:23 -0800

    Add the rest of the traps to the selections

--- src/Menus/TrapSelector.gd
+++ src/Menus/TrapSelector.gd
@@ -1,13 +1,40 @@
 extends Control
 
-export(String, "Spike", "Ice", "Lava", "Traps") var item
-export(Dictionary) var itemMap
+
+onready var spike_tile = load("res://Art/Traps/Spike.tres")
+onready var lava_tile = load("res://Art/Traps/Lava.tres")
+onready var ice_tile = load("res://Art/Traps/Ice.tres")
+onready var magic_missile_tile = load("res://Art/Traps/Magic Missile.tres")
+onready var monster_tile = load("res://Art/Traps/Monster.tres")
+onready var wind_tile = load("res://Art/Traps/Wind.tres")
+onready var crate_tile = load("res://Art/Traps/Crate.tres")
+onready var traps_tile = load("res://Art/Traps/Traps.tres")
+
+export(String, "Spike", "Lava", "Ice", "Magic Missile", "Monster", "Wind", "Crate", "Traps") var item
+
 
 export var freq = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Item.texture = itemMap[item]
+	match item:
+		"Spike":
+			$Item.texture = spike_tile
+		"Lava":
+			$Item.texture = lava_tile
+		"Ice":
+			$Item.texture = ice_tile
+		"Magic Missile":
+			$Item.texture = magic_missile_tile
+		"Monster":
+			$Item.texture = monster_tile
+		"Wind":
+			$Item.texture = wind_tile
+		"Crate":
+			$Item.texture = crate_tile
+		"Traps":
+			$Item.texture = traps_tile
+
 	$Item/Label/Slider.value = freq
 
 func _on_Slider_value_changed(value):
